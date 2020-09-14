95e562e6461fc16e94990b4996026266e8325d6e
dugwill <douglaswi1@comcast.net>
2020-09-14 15:15:29 -0600

    Initial commit

---
+++ event.go
@@ -0,0 +1,87 @@
+package libevent
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/Comcast/gots"
+	scte35 "github.com/Comcast/gots/scte35"
+)
+
+// Event is use to create a xml representation of the scte35 event
+type Event struct {
+	Time          time.Time                `xml:"manifestTime" json:"manifestTime,omitempty"`
+	StreamName    string                   `xml:"streamName,attr" json:"streamName,omitempty"`
+	PeriodID      string                   `xml:"periodId,attr" json:"upid,omitempty"`
+	EventTime     time.Time                `xml:"eventTime,attr" json:"eventTime,omitempty"`
+	EventID       uint32                   `xml:"eventID,attr" json:"eventID,omitempty"`
+	PTS           gots.PTS                 `xml:"pts,attr" json:"pts,omitempty"`
+	Command       scte35.SpliceCommandType `xml:"command,attr" json:"command,omitempty"`
+	TypeID        scte35.SegDescType       `xml:"typeID,attr" json:"typeID,omitempty"`
+	Signal        string                   `xml:"signal,attr" json:"signal,omitempty"`
+	BreakDuration gots.PTS                 `xml:"duration,attr" json:"duration,omitempty"`
+	TimeToSplice  uint64                   `xml:"timeToSplice,attr" json:"timeToSplice,omitempty"`
+	EventJSON     []byte                   `xml:"eventJSON,attr" json:"-"`
+}
+
+//JMarshalEvent returns a []byte containing the marshalled event
+func (e *Event) JMarshalEvent() (s []byte, err error) {
+
+	s, err = json.MarshalIndent(e, "  ", "  ")
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+//JUnMarshalEvent UnMarshals a []byte into an event struct
+func (e *Event) JUnMarshalEvent(s []byte) (err error) {
+
+	err = json.Unmarshal(s, e)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewEvent create an event instance and populates it with the decoded SCTE-35 data
+func NewEvent(d []byte) (e *Event, err error) {
+
+	scte, err := scte35.NewSCTE35(d)
+	if err != nil {
+		return nil, err
+	}
+
+	e = &Event{
+		PTS:     scte.PTS(),
+		Command: scte.Command(),
+	}
+
+	for _, desc := range scte.Descriptors() {
+		if desc.TypeID() >= 0x34 && desc.TypeID() <= 0x37 {
+			e.TypeID = desc.TypeID()
+			e.EventID = desc.EventID()
+			if desc.HasDuration() {
+				e.BreakDuration = desc.Duration()
+			}
+
+			// Set the event signal value, acconting for MID structure in descriptor
+			if desc.UPIDType() == scte35.SegUPIDADI {
+				e.Signal = fmt.Sprintf("%s", desc.UPID())
+
+			} else if desc.UPIDType() == scte35.SegUPIDMID {
+				for _, m := range desc.MID() {
+					if m.UPIDType() == scte35.SegUPIDADI {
+						e.Signal = fmt.Sprintf("%s", m.UPID())
+					}
+				}
+			} else {
+				err = fmt.Errorf("Could not find Signal")
+			}
+
+		}
+	}
+
+	return e, nil
+}
---
+++ event_test.go
@@ -0,0 +1,30 @@
+package libevent
+
+import (
+	"encoding/base64"
+	"fmt"
+	"testing"
+)
+
+var testScte35 = "/DBrAACHCXBcAP/wBQb/hYm0rABVAlNDVUVJ/////3//AAAUmXANPw8cdXJuOm5iY3VuaS5jb206YnJjOjQxNDcyMzM5NAkfU0lHTkFMOkdxQS16X2paWlY0QUFBQUFBQUFLQVE9PTQDAXIWYww="
+
+func TestNewEvent(t *testing.T) {
+
+	base64Bytes, _ := base64.StdEncoding.DecodeString(testScte35)
+	base64Bytes = append([]byte{0}, base64Bytes...)
+
+	event, err := NewEvent(base64Bytes)
+	if err != nil {
+		t.Log("ERROR parsing SCTE-35 data: ", err)
+		return
+	}
+	t.Log("Parse SCTE-35 successful. Updateing LastEventPTS")
+
+	fmt.Println("Event ID: ", event.EventID)
+	if event.EventID != 4294967295 {
+		t.Errorf("Incorrect Event ID\n   Expected: 4294967295  Actual: %X\n", event.EventID)
+	}
+
+	fmt.Println("Event: ", event.Signal)
+
+}
